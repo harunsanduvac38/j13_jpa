ca1c6325fdebc45c6a9d775cca86988478596d23
mananas <mananas@10.1.2.15>
2025-07-18 11:06:07 +0200

    10elemntcollection

---
+++ src/main/java/com/cursogetafe/jpa/ejemplo10elementcollection/Test01.java
@@ -0,0 +1,28 @@
+package com.cursogetafe.jpa.ejemplo10elementcollection;
+
+import com.cursogetafe.jpa.config.Config;
+
+import jakarta.persistence.EntityManager;
+
+public class Test01 {
+	public static void main(String[] args) {
+		EntityManager em = Config.getEmf().createEntityManager();
+		
+		ContactoConTelefonos c = em.find(ContactoConTelefonos.class, 10);
+		System.out.println(c.getApellidos() + " " + c.getNombre());
+		
+	
+		em.close();
+		
+		System.out.println(c.getIdContacto());
+		
+		System.out.println(".............");
+		System.out.println("ahora acceso a los telefonos");
+		System.out.println(c.getTelefonos());
+	
+		
+		
+		
+	}
+
+}
